70b592703af1ef527c7e2b6bff6c365c3f3e6f11
Lelton Borges <leltondev@gmail.com>
2021-11-24 21:15:41 -0300

    v2.1 with sql

--- web-db.sql
+++ web-db.sql
@@ -1,43 +1,9 @@
--- MySQL dump 10.19  Distrib 10.3.31-MariaDB, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: desafio
--- ------------------------------------------------------
--- Server version	10.3.31-MariaDB-0ubuntu0.20.04.1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE=''+00:00'' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE=''NO_AUTO_VALUE_ON_ZERO'' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Current Database: `desafio`
---
-
-CREATE
-DATABASE /*!32312 IF NOT EXISTS*/ `desafio` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-
-USE
-`desafio`;
-
---
--- Table structure for table `produto`
---
-
 DROP TABLE IF EXISTS `produto`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `produto`
-(
-    `id`    int(10) NOT NULL AUTO_INCREMENT,
-    `nome`  varchar(100) NOT NULL,
-    `preco` double(10, 0
-) NOT NULL,
+
+CREATE TABLE `produto` (
+                           `id` int(10) NOT NULL AUTO_INCREMENT,
+                           `nome` varchar(100) NOT NULL,
+                           `preco` double(10,0) NOT NULL,
   `peso` float(10,0) NOT NULL,
   `lote` int(10) NOT NULL,
   `categoria` int(10) NOT NULL,
@@ -48,29 +14,6 @@ CREATE TABLE `produto`
   `codigo_barra` float NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `produto_id_uindex` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `produto`
---
-
-LOCK
-TABLES `produto` WRITE;
-/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
-INSERT INTO `produto`
-VALUES (1, ''1 '', 1, 1, 1, 1, ''1 '', ''1 '', ''2021 - 11 - 24 '', ''2021 - 11 - 25 '', 1);
-/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
-UNLOCK
-TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-11-24 16:12:55
+INSERT INTO `produto` (`id`, `nome`, `preco`, `peso`, `lote`, `categoria`, `url`, `descricao`, `data_fabricacao`, `data_validade`, `codigo_barra`) VALUES (4,'Arroz Preto',4235,345,345,5,'https://m.media-amazon.com/images/I/81-Yw7YyRBL._AC_UL320_.jpg','O Arroz Parboilizado Camil passa por um processo que cozinha o grão dentro da sua própria casca, utilizando apenas água e calor, sem adição de produtos químicos. Assim, o grão absorve os nutrientes da casca do arroz, deixando-o mais nutritivo.','2021-11-05','2021-11-30',345345),(5,'Molho Tradicional',2,2,664,2,'https://images-na.ssl-images-amazon.com/images/I/71Aw85AxSGL._AC_UL160_SR160,160_.jpg','O molho de tomate heinz tradicional traz muito mais sabor e qualidade para as suas receitas.','2021-11-24','2021-11-11',345345),(6,'FEIJÃO CARIOCA KICALDO',23,4,54345,2,'https://images-na.ssl-images-amazon.com/images/I/815s35jNVkL._AC_UL160_SR160,160_.jpg','Com uma grande variedade de produtos, a Kicaldo preza sempre pela qualidade de cada um dos grãos para sempre oferecer saúde e bem-estar para as famílias brasileiras.','2020-12-28','2021-11-18',3636340),(7,'Leite Integral Piracanjuba',22,1,53,1,'https://images-na.ssl-images-amazon.com/images/I/8184YMgF4QL._AC_UL160_SR160,160_.jpg','Leite integral e estabilizantes trifosfato de sódio, citrato de sódio, monofosfato de sódio e difosfato de sódio. ALÉRGICOS: CONTÉM LEITE.CONTÉM LACTOSE.NÃO CONTÉM GLÚTEN.','2020-03-24','2021-11-01',35345),(8,'Feijão Preto Camill',25,3,35345,1,'https://images-na.ssl-images-amazon.com/images/I/51fSALNm7uL._AC_UL160_SR160,160_.jpg','Muito usado no dia a dia, o feijão preto conquistou seu lugar na mesa do brasileiro e habitualmente acompanha aquele delicioso arroz com sabor de família','2021-11-25','2022-01-24',43245300),(9,'Arroz',4235,345,345,5,'https://m.media-amazon.com/images/I/81-Yw7YyRBL._AC_UL320_.jpg','O Arroz Parboilizado Camil passa por um processo que cozinha o grão dentro da sua própria casca, utilizando apenas água e calor, sem adição de produtos químicos. Assim, o grão absorve os nutrientes da casca do arroz, deixando-o mais nutritivo.','2021-11-05','2021-11-30',345345),(10,'Molho Tradicional',2,2,664,2,'https://images-na.ssl-images-amazon.com/images/I/71Aw85AxSGL._AC_UL160_SR160,160_.jpg','O molho de tomate heinz tradicional traz muito mais sabor e qualidade para as suas receitas.','2021-11-24','2021-11-11',345345),(11,'FEIJÃO CARIOCA KICALDO',23,4,54345,2,'https://images-na.ssl-images-amazon.com/images/I/815s35jNVkL._AC_UL160_SR160,160_.jpg','Com uma grande variedade de produtos, a Kicaldo preza sempre pela qualidade de cada um dos grãos para sempre oferecer saúde e bem-estar para as famílias brasileiras.','2020-12-28','2021-11-18',3636340),(12,'Leite Integral Piracanjuba',22,1,53,1,'https://images-na.ssl-images-amazon.com/images/I/8184YMgF4QL._AC_UL160_SR160,160_.jpg','Leite integral e estabilizantes trifosfato de sódio, citrato de sódio, monofosfato de sódio e difosfato de sódio. ALÉRGICOS: CONTÉM LEITE.CONTÉM LACTOSE.NÃO CONTÉM GLÚTEN.','2020-03-24','2021-11-01',35345),(13,'Feijão Preto Camill',25,3,35345,1,'https://images-na.ssl-images-amazon.com/images/I/51fSALNm7uL._AC_UL160_SR160,160_.jpg','Muito usado no dia a dia, o feijão preto conquistou seu lugar na mesa do brasileiro e habitualmente acompanha aquele delicioso arroz com sabor de família','2021-11-25','2022-01-24',43245300);
